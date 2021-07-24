09f01a4bdb2443bb48cfdbff629efadea0a8bc8d
yxin <yxin@a10networks.com>
2021-07-24 01:32:24 -0400

    insert pipeline

--- .projenrc.js
+++ .projenrc.js
@@ -1,20 +1,21 @@
-const { web, AwsCdkTypeScriptApp } = require('projen');
+const { AwsCdkTypeScriptApp } = require('projen');
 
-const project = new AwsCdkTypeScriptApp({
-  authorAddress: 'damadden88@googlemail.com',
+const project = new AwsCdkTypeScriptApp({    
+  authorAddress: 'damadden88@googlemail.de',
   authorName: 'martin.mueller',
-  name: 'aws-cdk-todolist-ui',
-  defaultReleaseBranch: 'main',
   cdkVersion: '1.93.0',
   cdkVersionPinning: true,
+  defaultReleaseBranch: 'main',
+  jsiiFqn: 'projen.AwsCdkTypeScriptApp',
+  name: 'aws-cdk-todolist',
   cdkDependencies: [
+    '@aws-cdk/aws-dynamodb',
+    '@aws-cdk/aws-cognito',
     '@aws-cdk/aws-iam',
-    '@aws-cdk/aws-s3-deployment',
-    '@aws-cdk/aws-s3',
+    '@aws-cdk/aws-appsync',
     '@aws-cdk/aws-codepipeline',
     '@aws-cdk/aws-codepipeline-actions',
     '@aws-cdk/pipelines',
-    '@aws-cdk/aws-lambda-nodejs',
   ],
   deps: [
     'aws-cdk-staging-pipeline',
@@ -25,67 +26,6 @@ const project = new AwsCdkTypeScriptApp({
     '@aws-cdk/core:stackRelativeExports': true,
     '@aws-cdk/core:newStyleStackSynthesis': true,
   },
-  releaseWorkflow: false,
 });
-
-project.buildTask.prependExec('cd frontend && yarn install && yarn build && cd ..');
-
-project.setScript('cdkDeploy', 'cdk deploy');
-project.setScript('cdkDestroy', 'cdk destroy');
 
 project.synth();
-
-const frontendProject = new web.ReactTypeScriptProject({
-  defaultReleaseBranch: 'main',
-  outdir: 'frontend',
-  parent: project,
-  name: 'aws-cdk-todolist-ui-frontend',
-  deps: [
-    '@aws-amplify/auth',
-    '@aws-amplify/ui-components',
-    '@aws-amplify/ui-react',
-    'aws-amplify',
-    'react-query@^2',
-    'react-router',
-    'react-router-dom',
-    '@types/react-router-dom',
-  ],
-  devDeps: [
-    '@graphql-codegen/cli',
-    '@graphql-codegen/typescript',
-    '@graphql-codegen/typescript-operations',
-    '@graphql-codegen/typescript-react-query@alpha',
-    'amplify-graphql-docs-generator',
-    'aws-sdk@^2',
-    'graphql',
-  ],
-  tsconfig: {
-    compilerOptions: {
-      forceConsistentCasingInFileNames: false,
-      strictNullChecks: false,
-    },
-  },
-
-  releaseWorkflow: false,
-});
-
-frontendProject.addTask('dev', {
-  description: 'Runs the application locally',
-  exec: 'react-scripts start',
-});
-
-frontendProject.addTask('generate-exports', {
-  description: 'Generates aws-exports.js',
-  exec: 'node bin/generateExports.js dev && node bin/generateExports.js prod',
-});
-
-frontendProject.addTask('generate-statements', {
-  exec: 'node bin/generateStatements.js',
-});
-
-frontendProject.addTask('codegen', {
-  description: 'Generates frontend GraphQL wrapper API code',
-  exec: 'yarn run generate-statements && graphql-codegen --config codegen.yml',
-});
-
-frontendProject.synth();
--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,45 @@
 import * as core from '@aws-cdk/core';
 import { PipelineStack } from 'aws-cdk-staging-pipeline';
-import { StaticSite } from './static-site';
+import { AppSyncStack } from './appsync-stack';
 
 const app = new core.App();
 
-new PipelineStack(app, 'todolist-ui-pipeline', {
-  stackName: 'todolist-ui-pipeline',
+new PipelineStack(app, 'todolist-pipeline', {
+  stackName: 'todolist-pipeline',
   // Account and region where the pipeline will be build
   env: {
-    account: '981237193288',
-    region: 'eu-central-1',
+    account: '296025538260',
+    region: 'ap-northeast-1',
   },
   // Staging Accounts e.g. dev qa prod
   stageAccounts: [{
     account: {
-      id: '981237193288',
-      region: 'eu-central-1',
+      id: '296025538260',
+      region: 'ap-northeast-1',
     },
     stage: 'dev',
   }, {
     account: {
-      id: '991829251144',
-      region: 'eu-central-1',
+      id: '296025538260',
+      region: 'ap-northeast-1',
     },
     stage: 'prod',
   }],
-  branch: 'main',
-  repositoryName: 'aws-cdk-todolist-ui',
-  buildCommand: 'cd frontend && yarn install && yarn build && cd ..',
+  branch: 'appsync-stage',
+  repositoryName: 'aws-cdk-todolist',
   customStack: (scope, stageAccount) => {
-    const staticSite = new StaticSite(scope, `todolist-ui-stack-${stageAccount.stage}`, {
-      stackName: `todolist-ui-stack-${stageAccount.stage}`,
+    const appSyncStack = new AppSyncStack(scope, `todolist-stack-${stageAccount.stage}`, {
+      stackName: `todolist-stack-${stageAccount.stage}`,
       stage: stageAccount.stage,
     });
-    return staticSite;
+    return appSyncStack;
   },
-  // which stage needs a manual approval. Here is only prod
+  // all stages need manual approval
   manualApprovals: (stageAccount) => stageAccount.stage === 'prod',
-  // not much test magic here yet. Will soon setup some Postman integration tests Check the property for instructions!
-  testCommands: (stageAccount) => [
-    `echo "${stageAccount.stage} stage"`,
-    'STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" $bucketWebsiteUrl)',
-    'echo Statuscode = $STATUSCODE',
-    'if test $STATUSCODE -ne 200; then exit 1; fi',
-  ],
   gitHub: {
     owner: 'mmuller88',
-    oauthToken: core.SecretValue.secretsManager('cdktoken', {
-      jsonField: 'githubtoken',
+    oauthToken: core.SecretValue.secretsManager('alfcdk', {
+      jsonField: 'muller88-github-token',
     }),
   },
 });
